3c074d82ff32adc4823a7d746e8853e0c18c4f87
Ziragn <umbertomagalhaesbizzo@gmail.com>
2026-04-01 22:00:19 -0300

    Update registro_producao.py

--- model/registro_producao.py
+++ model/registro_producao.py
@@ -12,6 +12,14 @@ class RegistroProducao(Base):
     vaca_id = Column(Integer, ForeignKey('Vaca.pk_vaca'), nullable=False)
 
     def __init__(self, data_registro: date, litros: float, vaca_id: int):
+        """
+        Cria o registro de produção de uma vaca já existente na tabela Vaca.
+
+        Arguments:
+        data_registro: A data do registro.
+        litros: A produção de leite em litros daquele dia
+        vaca_id: O ID da vaca, que puxa de uma vaca da tabela Vaca.
+        """
         self.data_registro = data_registro
         self.litros = litros
         self.vaca_id = vaca_id
